2e17eac8aa0bd974cab37e5f4b1d3654b38527a1
jakehyo <jskh1999@gmail.com>
2018-04-13 13:16:44 -0700

    -_-

--- app/src/main/java/com/example/han/testtranslator/MainActivity.java
+++ app/src/main/java/com/example/han/testtranslator/MainActivity.java
@@ -2,36 +2,35 @@ package com.example.han.testtranslator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.google.api.GoogleAPI;
-import com.google.api.GoogleAPIException;
-import com.google.api.translate.Language;
-import com.google.api.translate.Translate;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String recognizedText;
+    public static String GOOGLE_API_KEY = "AIzaSyAeRI1ySyvjH-SZXcLPR8fRofgPNC9XKXE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-        // Set the HTTP referrer to your website address.
 
-        // Set the Google Translate API key
-        // See: http://code.google.com/apis/language/translate/v2/getting_started.html
-        GoogleAPI.setKey("AIzaSyAeRI1ySyvjH-SZXcLPR8fRofgPNC9XKXE");
 
-        String translatedText = null;
-        try {
-            translatedText = Translate.DEFAULT.execute("Bonjour le monde", Language.FRENCH, Language.ENGLISH);
-        } catch (GoogleAPIException e) {
-            e.printStackTrace();
-            Log.d("ERROR", "-_-");
+
+
+
+//        TessBaseAPI baseApi = new TessBaseAPI();
+//// DATA_PATH = Path to the storage
+//// lang = for which the language data exists, usually "eng"
+//        baseApi.init(DATA_PATH, lang);
+//// Eg. baseApi.init("/mnt/sdcard/tesseract/tessdata/eng.traineddata", "eng");
+//        baseApi.setImage(bitmap);
+//        recognizedText = baseApi.getUTF8Text();
+//        baseApi.end();
+
         }
-
-        Log.d("OnStart", "onCreate: " + translatedText);
 
 
 
     }
-}
+
